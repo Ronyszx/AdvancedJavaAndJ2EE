a1932e06df1beebb2d19ee4d1861bb0e627de6b9
Ronyszx <ronysouz7@gmail.com>
2022-06-14 23:46:17 +0530

    1st

--- src/SubStringExample.java
+++ src/SubStringExample.java
@@ -1,8 +1,5 @@
 public class SubStringExample{
     public static void main(String args[]){
-
     String s1="klecse";
-
     System.out.println(s1.substring(3,5));//returns cs System.out.println(s1.substring(3));//returns cse
-
 }}
